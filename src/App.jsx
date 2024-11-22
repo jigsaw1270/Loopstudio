24cfbddfbcfbda3e601951439cce226da2dbc95e
Mahmudul <mahmudul@blubnc.com>
2024-11-22 16:25:44 +0600

    responsive  grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import facebook from "./assets/images/icon-facebook.svg";
 import twitter from "./assets/images/icon-twitter.svg";
 import pinterest from "./assets/images/icon-pinterest.svg";
 import instragram from "./assets/images/icon-instagram.svg";
+import ham from "./assets/images/icon-hamburger.svg";
 
 function App() {
   return (
     <main>
-      <section className="relative bg-[url('./assets/images/desktop/image-hero.jpg')] w-full h-[40rem]  bg-no-repeat bg-cover bg-center">
+      <section className="relative bg-[url('./assets/images/desktop/image-hero.jpg')] md:w-full md:h-[40rem] h-[40rem] bg-no-repeat bg-cover bg-center">
         {/* Overlay */}
         <div className="absolute inset-0 bg-gray-900 bg-opacity-50 z-10"></div>
 
@@ -21,7 +22,8 @@ function App() {
                 loopstudios
               </h1>
             </div>
-            <ul className="flex space-x-6 font-alata text-sm">
+            <img src={ham} alt="" className="md:hidden block" />
+            <ul className="md:flex hidden space-x-6 font-alata text-sm">
               <li className="text-white">About</li>
               <li className="text-white">Careers</li>
               <li className="text-white">Events</li>
@@ -31,19 +33,19 @@ function App() {
           </nav>
 
           {/* Hero Text */}
-          <div className="uppercase font-jose text-7xl font-light text-white p-8 tracking-widest border-white border-2 mt-32 w-[60%]">
+          <div className="uppercase font-jose text-4xl md:text-7xl font-light text-white p-8 tracking-widest border-white border-2 mt-32 md:w-[60%]">
             Immersive <br /> Experiences <br /> that deliver
           </div>
         </div>
       </section>
-      <section className="my-[15rem]">
+      <section className="md:my-[15rem] my-12">
         <div className="relative container">
           {/* Image */}
           <img src={interactive} alt="interactive" className="w-[50rem]" />
 
           {/* Overlay Content */}
-          <div className="absolute -bottom-1 right-[10rem] bg-white px-20 pt-20 max-w-screen-xl">
-            <h1 className="uppercase font-jose text-5xl font-light">
+          <div className="md:absolute md:-bottom-1 md:right-[10rem] bg-white md:px-20 md:pt-20 pt-8 max-w-screen-xl text-center">
+            <h1 className="uppercase font-jose text-3xl font-light">
               the leader in <br /> interactive vr
             </h1>
             <p className="font-alata text-dark-gray mt-4">
@@ -58,16 +60,16 @@ function App() {
 
       <section>
         <div className="container flex items-center justify-between mb-20">
-          <h1 className="uppercase font-jose text-5xl font-light">
+          <h1 className="uppercase font-jose text-4xl font-light max-sm:mx-auto">
             Our creations
           </h1>
-          <button className="py-3 px-10 border-2 border-black font-alata text-sm tracking-widest">
+          <button className="py-3 px-10 border-2 border-black font-alata text-sm tracking-widest hidden md:block">
             SEE ALL
           </button>
         </div>
 
-        <div className="container mb-8 grid grid-cols-4">
-          <div className="relative bg-[url('/src/assets/images/desktop/image-deep-earth.jpg')] max-w-[17rem] h-[30rem] bg-no-repeat">
+        <div className="container mb-8 grid md:grid-cols-4 grid-cols-1">
+          <div className="relative bg-[url('/src/assets/images/desktop/image-deep-earth.jpg')] md:w-[17rem] md:h-[30rem] h-28 w-[20rem] bg-no-repeat">
             <p className="text-white absolute bottom-8 text-3xl p-8 font-jose font-light">
               DEEP <br />
               EARTH
